6de9f043d81b72201a38312992814e2fb1e9108a
hrvojefuri <93526792+hrvojefuri@users.noreply.github.com>
2021-11-17 18:41:32 +0100

    Dodao charset i novi unos

--- githubprimjer.sql
+++ githubprimjer.sql
@@ -1,7 +1,9 @@
 drop database if exists githubprimjer;
-create database githubprimjer;
+create database githubprimjer character set utf8;
 use githubprimjer;
 create table git (
     sifra omt not null primary key auto_increment,
     naziv varchar(50) not null
 );
+
+insert into git(sifra,naziv) values (null,'Test');
