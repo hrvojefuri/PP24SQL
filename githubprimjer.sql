2dceb22fa58785e656a8c56fcd1d42977fcdbdb7
hrvojefuri <93526792+hrvojefuri@users.noreply.github.com>
2021-11-17 18:51:51 +0100

    Dodao ERA dijagram

--- githubprimjer.sql
+++ githubprimjer.sql
@@ -2,7 +2,7 @@ drop database if exists githubprimjer;
 create database githubprimjer character set utf8;
 use githubprimjer;
 create table git (
-    sifra omt not null primary key auto_increment,
+    sifra int not null primary key auto_increment,
     naziv varchar(50) not null
 );
 
