eddbf55eabb93f597818cb33211e83e59ce1c4bd
hrvojefuri <93526792+hrvojefuri@users.noreply.github.com>
2021-11-23 19:31:16 +0100

    brisanje tablica i preraspodjela atributa
    obrisane tablce: osoba, adresa, dostava, adresadostave i placanje; preraspodijeljeni atributi iz navedenih tablica u postojeće tablice

--- zavrsnirad/furiousvortex.sql
+++ zavrsnirad/furiousvortex.sql
@@ -6,73 +6,44 @@ create database furiousvortex;
 use furiousvortex;
 
 create table kupac (
-    sifra   int not null primary key auto_increment,
-    osoba   int not null,
-    adresa  int not null
-);
-
-create table osoba (
-    sifra       int not null primary key auto_increment,
-    ime         varchar(50) not null,
-    prezime     varchar(50) not null,
-    adresa      int not null,
-    email       varchar(50)
-);
-
-create table adresa (
     sifra           int not null primary key auto_increment,
+    ime             varchar(50) not null,
+    prezime         varchar(50) not null,
     ulica           varchar(50) not null,
     kucnibroj       varchar(5) not null,
     grad            varchar(50) not null,
-    postanskibroj   char(5) not null
+    postanskibroj   char(5) not null,
+    email           varchar(50)
 );
 
 create table narudzba (
     sifra           int not null primary key auto_increment,
     kupac           int not null,
     iznos           decimal(18,2) not null,
-    dostava         int not null,
     datumnarudzbe   datetime not null,
-    placanje        int not null
-);
-
-create table dostava (
-    sifra           int not null primary key auto_increment,
-    adresadostave   int not null,
+    nacinplacanja   varchar(50) not null,
     dostavnasluzba  varchar(50) not null,
-    datumdostave    datetime
-);
-
-create table adresadostave (
-    sifra           int not null primary key auto_increment,
-    ulica           varchar(50) not null,
-    kucnibroj       varchar(5) not null,
-    grad            varchar(50) not null,
-    postanskibroj   char(5) not null
-);
-
-create table placanje (
-    sifra int not null primary key auto_increment,
-    vrsta varchar(50) not null,
-    brojkartice char(16),
-    cvv char(3)
+    datumdostave    datetime,
+    isporuceno      boolean
 );
 
 create table kosarica (
     sifra       int not null primary key auto_increment,
     narudzba    int not null,
     proizvod    int not null,
-    kolicina    char(2) not null
+    kolicina    char(2) not null,
+    cijenasuma  decimal(18,2) not null
 );
 
 create table proizvod (
     sifra           int not null primary key auto_increment,
-    naziv           varchar(50) not null,
-    kategorija      int not null,
+    zanr            int not null,
     izvodac         int not null,
+    naziv           varchar(50) not null,
     cijena          decimal(18,2) not null,
     izdavackakuca   varchar(50) not null,
-    zaliha          char(4) not null
+    naslovnica      varchar(50),
+    zaliha          int not null
 );
 
 create table izvodac (
@@ -81,22 +52,7 @@ create table izvodac (
     web     varchar(50)
 );
 
-create table kategorija (
+create table zanr (
     sifra   int not null primary key auto_increment,
     naziv   varchar(50) not null
 );
-
-alter table kupac add foreign key (osoba) references osoba(sifra);
-alter table kupac add foreign key (adresa) references adresa(sifra);
-
-alter table narudzba add foreign key (kupac) references kupac(sifra);
-alter table narudzba add foreign key (dostava) references dostava(sifra);
-alter table narudzba add foreign key (placanje) references placanje(sifra);
-
-alter table dostava add foreign key (adresadostave) references adresadostave(sifra);
-
-alter table kosarica add foreign key (narudzba) references narudzba(sifra);
-alter table kosarica add foreign key (proizvod) references proizvod(sifra);
-
-alter table proizvod add foreign key (kategorija) references kategorija(sifra);
-alter table proizvod add foreign key (izvodac) references izvodac(sifra);
