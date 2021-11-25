5f6d0d512be7713b7a31657ee44412754b05435c
hrvojefuri <93526792+hrvojefuri@users.noreply.github.com>
2021-11-25 20:48:04 +0100

    dodavanje inserata od 15_11_2021

--- zadaci/skriptapp24/skriptapp24.sql
+++ zadaci/skriptapp24/skriptapp24.sql
@@ -58,3 +58,108 @@ alter table polaznik add foreign key (osoba) references osoba(sifra);
 alter table clan add foreign key (grupa) references grupa(sifra);
 alter table clan add foreign key (polaznik) references polaznik(sifra);
 
+# Loš način
+# 1
+insert into smjer values (null,'PHP programiranje',130,5999.99,false);
+
+# Bolji način
+# 2
+insert into smjer (naziv,trajanje) values ('Java programiranje',130);
+
+# Primjer dobre prakse način
+# 3
+insert into smjer (sifra,naziv,trajanje,cijena,certificiran)
+values (null,'Računovodstvo',180,null,true);
+
+# 1
+insert into osoba (sifra,ime,prezime,oib,email)
+values (null,'Tomislav','Jakopec','97415127262','tjakopec@gmail.com');
+
+# unesite sebe
+# 2 - 21
+insert into osoba (sifra,ime,prezime,oib,email) values
+(null,'Tomislav','Sabolić',null,'sabolic55@gmail.com'),
+(null,'Kristijan','Hegeduš',null,'kico.kiki@gmail.com'),
+(null,'Matej','Papac',null,'papac.tmrip@gmail.com'),
+(null,'Mladen','Božić',null,'mbozic987@gmail.com'),
+(null,'Josip','Ervačić',null,'jokijoki7@gmail.com'),
+(null,'Grgo','Kvesić',null,'grgo0203@gmail.com'),
+(null,'Dušan','Strajinić',null,'dusanstrajinic9@gmail.com'),
+(null,'Filip','Đunda',null,'filip.djunda1@gmail.com'),
+(null,'Marija','Mikić',null,'marija.snowml@gmail.com'),
+(null,'Hrvoje','Furi',null,'hrvoje.furi@gmail.com'),
+(null,'Roman','Grubić',null,'grubic.roman@gmail.com'),
+(null,'Magdalena','Janić',null,'lenchi.janic@gmail.com'),
+(null,'Luka','Grigić',null,'luka.grigic94@gmail.com'),
+(null,'Dario','Šalić',null,'dasalic1@gmail.com'),
+(null,'Jakob','Brkić',null,'jale.pnv@gmail.com'),
+(null,'Stefan','Lazarević',null,'slazar81095@gmail.com'),
+(null,'Kristina','Galić',null,'drozdek.kristina@gmail.com'),
+(null,'Mateo','Opančar',null,'opancarmateo@gmail.com'),
+(null,'Ante','Dragičević',null,'dragicevicante7@gmail.com'),
+(null,'Luka','Runje',null,'luka.runje@hotmail.com');
+
+
+# 1
+insert into predavac (sifra,osoba,iban) values (null,1,null);
+
+
+
+# 1 - 20
+insert into polaznik (sifra,osoba,brojugovora) values
+(null,2,''),
+(null,3,''),
+(null,4,''),
+(null,5,''),
+(null,6,''),
+(null,7,''),
+(null,8,''),
+(null,9,''),
+(null,10,''),
+(null,11,''),
+(null,12,''),
+(null,13,''),
+(null,14,''),
+(null,15,''),
+(null,16,''),
+(null,17,''),
+(null,18,''),
+(null,19,''),
+(null,20,''),
+(null,21,'');
+
+
+# 1
+insert into grupa (sifra,naziv,smjer,predavac,datumpocetka)
+values (null,'PP24',1,1,'2021-10-27');
+
+# 2
+insert into grupa (sifra,naziv,smjer,predavac,datumpocetka)
+values (null,'JP25',2,null,'2021-10-27');
+
+# 3
+insert into grupa (sifra,smjer,naziv,predavac)
+values (null,3,'R12',null);
+
+
+insert into clan(grupa,polaznik) values
+(1,1),
+(1,2),
+(1,3),
+(1,4),
+(1,5),
+(1,6),
+(1,7),
+(1,8),
+(1,9),
+(1,10),
+(1,11),
+(1,12),
+(1,13),
+(1,14),
+(1,15),
+(1,16),
+(1,17),
+(1,18),
+(1,19),
+(1,20);
